cb129bd1c8ba0dba9c233c8e2eff4b3ea6154f8f
icymort <icymort@gmail.com>
2024-02-18 18:57:22 +0200

    Init projects

---
+++ src/Controller/MoviesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MoviesController extends AbstractController
+{
+    #[Route('/movies', name: 'app_movies')]
+    public function index(): Response
+    {
+        $movies = ["Avangers: Endgame", "Inception", "Loki", "Black Widow"];
+
+        return $this->render('index.html.twig', array(
+            'movies' => $movies
+        ));
+    }
+}
